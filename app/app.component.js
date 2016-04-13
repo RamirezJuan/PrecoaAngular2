4f0c8ba1f9df6776622201a7d1b178c52d28be63
Juan Ramirez <jramirez@precoa.com>
2016-04-12 17:47:58 -0700

    Dispalayed top four heroes

--- app/app.component.js
+++ app/app.component.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core', './heroes.component', './hero.service', 'angular2/router'], function(exports_1, context_1) {
+System.register(['angular2/core', './heroes.component', './hero.service', 'angular2/router', './dashboard.component'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,7 +10,7 @@ System.register(['angular2/core', './heroes.component', './hero.service', 'angul
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1, heroes_component_1, hero_service_1, router_1;
+    var core_1, heroes_component_1, hero_service_1, router_1, dashboard_component_1;
     var AppComponent;
     return {
         setters:[
@@ -25,6 +25,9 @@ System.register(['angular2/core', './heroes.component', './hero.service', 'angul
             },
             function (router_1_1) {
                 router_1 = router_1_1;
+            },
+            function (dashboard_component_1_1) {
+                dashboard_component_1 = dashboard_component_1_1;
             }],
         execute: function() {
             AppComponent = (function () {
@@ -34,7 +37,7 @@ System.register(['angular2/core', './heroes.component', './hero.service', 'angul
                 AppComponent = __decorate([
                     core_1.Component({
                         selector: 'my-app',
-                        template: "\n  <h1>{{title}}</h1>\n  <a [routerLink]=\"['Heroes']\">Heroes</a>\n  <router-outlet></router-outlet>\n  ",
+                        template: "\n  <h1>{{title}}</h1>\n  <nav>\n    <a [routerLink]=\"['Dashboard']\">Dashboard</a>\n    <a [routerLink]=\"['Heroes']\">Heroes</a>\n  </nav>\n  <router-outlet></router-outlet>\n  ",
                         directives: [router_1.ROUTER_DIRECTIVES],
                         providers: [
                             router_1.ROUTER_PROVIDERS,
@@ -42,6 +45,12 @@ System.register(['angular2/core', './heroes.component', './hero.service', 'angul
                         ]
                     }),
                     router_1.RouteConfig([
+                        {
+                            path: '/dashboard',
+                            name: 'Dashboard',
+                            component: dashboard_component_1.DashboardComponent,
+                            useAsDefault: true
+                        },
                         {
                             path: '/heroes',
                             name: 'Heroes',
--- app/app.component.ts
+++ app/app.component.ts
@@ -2,14 +2,17 @@ import {Component} from 'angular2/core';
 import {HeroesComponent} from './heroes.component';
 import {HeroService} from './hero.service';
 import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from 'angular2/router';
-import {DashboardComponent} from './dashboard.component.ts'
+import {DashboardComponent} from './dashboard.component'
 
 @Component(
 {
   selector: 'my-app',
   template:`
   <h1>{{title}}</h1>
-  <a [routerLink]="['Heroes']">Heroes</a>
+  <nav>
+    <a [routerLink]="['Dashboard']">Dashboard</a>
+    <a [routerLink]="['Heroes']">Heroes</a>
+  </nav>
   <router-outlet></router-outlet>
   `,
   directives: [ROUTER_DIRECTIVES],
@@ -20,6 +23,12 @@ import {DashboardComponent} from './dashboard.component.ts'
 })
 
 @RouteConfig([
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: DashboardComponent,
+    useAsDefault: true
+  },
   {
     path: '/heroes',
     name: 'Heroes',
